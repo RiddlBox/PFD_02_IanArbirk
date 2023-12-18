32915937184fe60e77659348692de80cbee1b434
#Ian Arbirk# <115164080+RiddlBox@users.noreply.github.com>
2023-12-18 14:42:17 +0100

    Character can finally move

--- PFD_02/Assets/Scripts/PlayerMovement.cs
+++ PFD_02/Assets/Scripts/PlayerMovement.cs
@@ -7,23 +7,24 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController2D controller;
     public float runSpeed = 40f;
     float horizontalMove = 0f;
+    bool jump = false;
     // 12:21 https://www.youtube.com/watch?v=dwcT-Dch0bA 
 
-
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+     
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump = true;
+        }
     }
     void FixedUpdate () // Fixed update ideal for physics. Called a fixed amount of time pr. sec
     {
         controller.Move(horizontalMove * Time.fixedDeltaTime, false, false); // Moving the character 
+        jump = false;
     }
 }
